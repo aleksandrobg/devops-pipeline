990a87fd97379c70ec09638e3963ae9be610ee94
aleksandrobg <129070172+aleksandrobg@users.noreply.github.com>
2023-03-27 18:50:52 -0300

    Criado arquivo para teste calculadora
    testar calculadora.py

--- test_calculadora.py
+++ test_calculadora.py
@@ -10,7 +10,7 @@ def test_subtract():
     c = Calculadora()
     assert c.subtract(5, 3) == 2
 
-def test_multiply():
+    def test_multiply():
     c = Calculadora()
     assert c.multiply(2, 3) == 6
 
